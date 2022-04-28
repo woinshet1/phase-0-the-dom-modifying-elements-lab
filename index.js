59f1b64879596f854a9478a200c334628bbca96e
Woinshet <woinshet1>
2022-04-28 16:46:55 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Woinshet is the champion";
+document.body.appendChild(newHeader);
